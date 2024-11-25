b6601b3f12615d5ce56963169b44fb7f794fa298
Alevtin <antonov3017@gmail.com>
2024-11-25 16:52:22 +0300

    dialog date

--- controller/start_window.py
+++ controller/start_window.py
@@ -3,6 +3,7 @@ from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import QMessageBox
 import logging
 
+from controller.date_dialog import DateDialog
 from controller.functions import get_lesson_names, get_team_names, validate_and_convert_date
 from model.db_connect import DatabaseConnector
 from view.main_window import Ui_MainWindow
@@ -21,10 +22,10 @@ class StartWindow(qtw.QMainWindow, Ui_MainWindow):
             return  # Если подключение не удалось, выходим из конструктора
 
         self.tbl_contracts_view()
-        # self.refresh_table()
         self.setup_tbl_contracts_view()
 
         self.tableWidget_Contracts.itemChanged.connect(self.on_item_changed)
+        self.tableWidget_Contracts.cellDoubleClicked.connect(self.on_cell_double_clicked)
 
     def tbl_contracts_view(self):
         # Создаем модели для таблиц
@@ -34,6 +35,8 @@ class StartWindow(qtw.QMainWindow, Ui_MainWindow):
 
         self.tableWidget_Contracts.setRowCount(0)  # Сбрасываем количество строк
         self.tableWidget_Contracts.setColumnCount(self.contracts_model.columnCount())  # Устанавливаем количество столбцов
+        # Отключаем отображение номеров строк
+        self.tableWidget_Contracts.verticalHeader().setVisible(False)
 
         # Заполнение QTableWidget данными из модели
         for row in range(self.contracts_model.rowCount()):
@@ -42,7 +45,7 @@ class StartWindow(qtw.QMainWindow, Ui_MainWindow):
                 item_data = self.contracts_model.data(self.contracts_model.index(row, column))
 
                 item = qtw.QTableWidgetItem()
-                if column in [1, 2, 3, 4, 5]:  # Запрет редактирования для 1-5 колонок
+                if column in [1, 2, 3, 4, 5, 8]:  # Запрет редактирования для 1-5 колонок
                     item.setText(str(item_data))  # Для остальных значений
                     item.setFlags(item.flags() & ~Qt.ItemFlag.ItemIsEditable)  # Убираем флаг редактирования
                 else:
@@ -69,17 +72,17 @@ class StartWindow(qtw.QMainWindow, Ui_MainWindow):
 
         if column in [7, 9]:  # для checkbox колонок
             new_state = item.checkState() == Qt.CheckState.Checked
-            logging.debug(f'Checkbox state changed at row {row}, column {column}. New state: {new_state}')
+            # logging.debug(f'Checkbox state changed at row {row}, column {column}. New state: {new_state}')
             self.update_database(row, column, new_state)
         else:  # для текстовых ячеек
             new_value = item.text()  # получаем новое текстовое значение
-            logging.debug(f'Text value changed at row {row}, column {column}. New value: {new_value}')
+            # logging.debug(f'Text value changed at row {row}, column {column}. New value: {new_value}')
             self.update_database(row, column, new_value)
 
     def handle_date_input(self, input_value):
-        # input_value = date_edit.text()
-        new_date = validate_and_convert_date(input_value)
-        if "Неверный формат даты" not in new_date and "Неверная дата" not in new_date:
+        if input_value:
+            new_date = validate_and_convert_date(input_value)
+        if ("Неверный формат даты" not in new_date and "Неверная дата" not in new_date) or new_date in ["", None]:
             return new_date
             # date_edit.setDate(QtCore.QDate.fromString(new_date, "dd.MM.yyyy"))
         else:
@@ -103,7 +106,7 @@ class StartWindow(qtw.QMainWindow, Ui_MainWindow):
             }
 
             column_name = column_map.get(column)
-            logging.debug(f'Column index: {column}, Column name: {column_name}')
+            # logging.debug(f'Column index: {column}, Column name: {column_name}')
             if column_name is None:
                 logging.error(f"Invalid column index: {column}")
                 return  # Не продолжаем, если индекс невалидный
@@ -117,8 +120,13 @@ class StartWindow(qtw.QMainWindow, Ui_MainWindow):
 
             query = QtSql.QSqlQuery()
             query.prepare(f"UPDATE contracts SET {column_name} = :value WHERE contract_id = :id;")
-            query.bindValue(":value", new_value)
+            print(f"1 update_database {new_value=}")
+            if new_value is None:
+                query.bindValue(":value", None)  # Устанавливаем NULL в запрос
+            else:
+                query.bindValue(":value", new_value)
             query.bindValue(":id", contract_id)
+            print(f"2 update_database {new_value=}")
 
             if not query.exec():
                 logging.error(f"Failed to update record: {query.lastError().text()}")
@@ -126,7 +134,6 @@ class StartWindow(qtw.QMainWindow, Ui_MainWindow):
                 return False
             else:
                 logging.debug("Record updated successfully.")
-                self.refresh_table()
 
         except Exception as e:
             logging.error(f"Ошибка при обновлении базы данных: {e}")
@@ -146,47 +153,23 @@ class StartWindow(qtw.QMainWindow, Ui_MainWindow):
         self.tableWidget_Contracts.setHorizontalHeaderLabels(['ID', 'Номер', 'Дата', 'Ребенок', 'Кружок', 'Группа',
                                                               'Примечание', 'Подп.', 'Дата раст.', 'Согл.подп.'])
 
-    def refresh_table(self):
-        """Обновляет данные в tableWidget_Contracts из базы данных."""
-        logging.debug("Refreshing table data from the database.")
+    def on_cell_double_clicked(self, row, column):
+        if column in [8,]:  # Проверяем, что это 8 колонка
+            current_value = self.tableWidget_Contracts.item(row, column).text() # Получаем текущее значение ячейки
+            dialog = DateDialog(current_value)  # Создаем диалог с текущим значением
+            if dialog.exec() == qtw.QDialog.DialogCode.Accepted:  # Проверяем, нажал ли пользователь "ОК"
+                new_value = dialog.get_value()  # Получаем новое значение из диалога
+                self.update_date(row, column, new_value)  # Обновляем значение ячейки
 
-        # Очищаем текущее содержимое таблицы
-        self.tableWidget_Contracts.clearContents()
-        self.tableWidget_Contracts.setRowCount(0)  # Убедитесь, что количество строк сброшено
-
-        # Создаем и настраиваем модель для данных
-        self.contracts_model = QtSql.QSqlTableModel(self)
-        self.contracts_model.setTable('contracts_view')
-        if not self.contracts_model.select():  # Извлекаем данные из таблицы
-            logging.error(f"Failed to select data from contracts_view: {self.contracts_model.lastError().text()}")
-            return
-
-        self.tableWidget_Contracts.setColumnCount(
-            self.contracts_model.columnCount())  # Устанавливаем количество столбцов
-
-        # Заполняем QTableWidget данными из модели
-        for row in range(self.contracts_model.rowCount()):
-            self.tableWidget_Contracts.insertRow(row)  # Вставляем новую строку
-            for column in range(self.contracts_model.columnCount()):
-                item_data = self.contracts_model.data(self.contracts_model.index(row, column))
-                logging.debug(
-                    f'{range(self.contracts_model.rowCount())=} {range(self.contracts_model.columnCount())=} {item_data=}')
-
-                item = qtw.QTableWidgetItem()  # Создаем элемент для таблицы
-
-                if isinstance(item_data, bool):  # Если это логическое значение
-                    logging.debug('Creating checkbox item.')
-                    item.setCheckState(Qt.CheckState.Checked if item_data else Qt.CheckState.Unchecked)
-                    item.setFlags(item.flags() | Qt.ItemFlag.ItemIsUserCheckable | Qt.ItemFlag.ItemIsEditable |
-                                  Qt.ItemFlag.ItemIsSelectable)
-                else:
-                    logging.debug('Creating text item.')
-                    item.setText(str(item_data))  # Для остальных значений
-                    item.setFlags(item.flags() | Qt.ItemFlag.ItemIsEditable | Qt.ItemFlag.ItemIsSelectable)
-
-                logging.debug(f'Inserting item into table at row {row}, column {column}.')
-                self.tableWidget_Contracts.setItem(row, column, item)  # Устанавливаем элемент в таблице
-
-        # Включаем сортировку
-        self.tableWidget_Contracts.setSortingEnabled(True)
-        logging.debug("Table refreshed successfully.")
+    def update_date(self, row, column, new_value):
+        if new_value not in ['', None]:
+            new_value = validate_and_convert_date(new_value)
+        else:
+            new_value = None
+        if ("Неверный формат даты" not in new_value and "Неверная дата" not in new_value) or new_value in ["", None]:
+            self.tableWidget_Contracts.item(row, column).setText(new_value)
+            logging.debug(f'date OK {new_value=} before update_database')
+            self.update_database(row, column, new_value)
+        else:
+            logging.debug(f'Ошибка в handle_date_input {new_value=}!!!')
+            return None
